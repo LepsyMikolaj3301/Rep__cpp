e3b9aeb01861d8e9a62c9cba7b8adceeb0dc948c
Mikolaj Lepsy <miki2004200@wp.pl>
2023-12-13 21:57:28 +0100

    day2 part 1 done

--- Advent_of_Code/2023/day2/day_2.cpp
+++ Advent_of_Code/2023/day2/day_2.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <regex>
+#include <sstream>
 using namespace std;
 
 struct game_data
@@ -26,30 +26,34 @@ string between(
 }
 
 
+int all_cube_val(string line, string color_cube) {
+    stringstream stream_line(line);
+    string s;
+    vector<string> v;
+    int value_max = 0, possible_val;
 
-game_data data_outta_string(string line) {
-    
+    while(getline(stream_line, s, ' ')) {
+        v.push_back(s);
+    }
+    for (int i = 2; i < v.size(); i++) {
+        if(v[i].find(color_cube) != string::npos) {
+            possible_val = stoi(v[i - 1]);
+            if(possible_val > value_max) {
+                value_max = possible_val;
+            }
+        }
+    }
+    return value_max;
+}
+
+
+game_data line_data_to_struct(string line) {
     game_data one_line_data;
-    
-
-    /*
-    index    |   color
-        0    |   red
-        1    |   blue
-        2    |   green
-    */ 
-    vector<int> spheres[3];
-
-    regex   reg_blue("(\d+)\s+blue"), 
-            reg_red("(\d+)\s+red"), 
-            reg_green("(\d+)\s+green");
-
     one_line_data.num_game = stoi(between(line, "Game ", ": "));
-    one_line_data.num_red = 
-
-
-
-
+    one_line_data.num_red = all_cube_val(line, "red");
+    one_line_data.num_green = all_cube_val(line, "green");
+    one_line_data.num_blue = all_cube_val(line, "blue");
+    return one_line_data;
 }
 
 
@@ -57,11 +61,19 @@ game_data data_outta_string(string line) {
 
 
 int main() {
+
+    game_data optimal_cube_values = {0, 12, 13, 14}, line_data;
+    int sum_of_ids = 0;
+
+
     fstream file("input.txt");
     string the_line;
     while(getline(file, the_line)) {
-
+        line_data = line_data_to_struct(the_line);
+        if(optimal_cube_values.num_red < line_data.num_red) continue;
+        if(optimal_cube_values.num_green < line_data.num_green) continue;
+        if(optimal_cube_values.num_blue < line_data.num_blue) continue;
+        sum_of_ids += line_data.num_game;
     }
-
-
+    cout << "Suma ID gier: " << sum_of_ids << '\n';
 }
